d69d0620563309bc395ceaa1a14039e3b6974cfa
david jaramillo <david.jaramillo.c@gmail.com>
2019-10-18 11:27:33 -0300

    update

--- test/MultisendBase2.test.js
+++ test/MultisendBase2.test.js
@@ -1,4 +1,5 @@
 const { BN, balance, ether, should, shouldFail, time, expectEvent } = require('openzeppelin-test-helpers');
+const truffleAssert = require('truffle-assertions');
 
 const MultisendBase = artifacts.require('MultisendBase2');
 require('chai')
@@ -41,10 +42,18 @@ contract('MultisendBase2', accounts => {
         
         await this._mutisend.bulkSendEth(_dest,_ethFee,_desAmount,_feedAmount ,{ from: _user, value:_total });
   
+
+
         const postbalanceUser = await web3.eth.getBalance(_user)
         const postbalanceDest = await web3.eth.getBalance(_dest)
         const postbalanceEthFee = await web3.eth.getBalance(_ethFee)
 
+
+        const destBalance=new BN(prebalanceDest).add(_desAmount);
+        new BN(postbalanceDest).should.be.a.bignumber.that.equals(`${destBalance}`);
+
+        const feeBalance=new BN(prebalanceEthFee).add(_feedAmount);
+        new BN(postbalanceEthFee).should.be.a.bignumber.that.equals(`${feeBalance}`);
 
         console.log("prebalanceDest:"+prebalanceDest)
         console.log("postbalanceDest:"+postbalanceDest)
@@ -59,6 +68,74 @@ contract('MultisendBase2', accounts => {
 
 
     });
+
+
+    it('should split money, over total', async function() {
+      const _desAmount= new BN("200000");
+      const _feedAmount= new BN("300000");
+      const _total= new BN("600000");
+      
+
+      const prebalanceDest = await web3.eth.getBalance(_dest)
+      const prebalanceUser = await web3.eth.getBalance(_user)
+      const prebalanceEthFee = await web3.eth.getBalance(_ethFee)
+      
+      await truffleAssert.reverts(this._mutisend.bulkSendEth(_dest,_ethFee,_desAmount,_feedAmount ,{ from: _user, value:_total }), "There are not enough funds");
+        
+      const postbalanceUser = await web3.eth.getBalance(_user)
+      const postbalanceDest = await web3.eth.getBalance(_dest)
+      const postbalanceEthFee = await web3.eth.getBalance(_ethFee)
+
+
+      console.log("prebalanceDest:"+prebalanceDest)
+      console.log("postbalanceDest:"+postbalanceDest)
+
+
+      console.log("prebalanceUser:"+prebalanceUser)
+      console.log("postbalanceUser:"+postbalanceUser)
+
+
+      console.log("prebalanceEthFee:"+prebalanceEthFee)
+      console.log("postbalanceEthFee:"+ postbalanceEthFee)
+
+
+  });
+
+
+
+  it('should split money, under total', async function() {
+    const _desAmount= new BN("200000");
+    const _feedAmount= new BN("300000");
+    const _total= new BN("600000");
+    
+
+    const prebalanceDest = await web3.eth.getBalance(_dest)
+    const prebalanceUser = await web3.eth.getBalance(_user)
+    const prebalanceEthFee = await web3.eth.getBalance(_ethFee)
+    
+    await truffleAssert.reverts(this._mutisend.bulkSendEth(_dest,_ethFee,_desAmount,_feedAmount ,{ from: _user, value:_total }), "There are not enough funds");
+        
+
+
+    const postbalanceUser = await web3.eth.getBalance(_user)
+    const postbalanceDest = await web3.eth.getBalance(_dest)
+    const postbalanceEthFee = await web3.eth.getBalance(_ethFee)
+
+    console.log()
+
+    console.log("prebalanceDest:"+prebalanceDest)
+    console.log("postbalanceDest:"+postbalanceDest)
+
+
+    console.log("prebalanceUser:"+prebalanceUser)
+    console.log("postbalanceUser:"+postbalanceUser)
+
+
+    console.log("prebalanceEthFee:"+prebalanceEthFee)
+    console.log("postbalanceEthFee:"+ postbalanceEthFee)
+
+
+});
 
 
   });
